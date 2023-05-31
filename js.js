3746559f9009669836fc8e6de77a1371c50ce5e6
AMentalCoderJ <josuesodre36@gmail.com>
2023-05-31 15:21:20 -0300

    1 commit

--- js.js
+++ js.js
@@ -77,3 +77,17 @@ liEducation.forEach((event, index)=>{
 sobreMin();
 divEducation[0].classList.add('ativo');
 divEducation[0].classList.add('ativo');
+
+
+function menuMobile(){
+    const activeMenu = document.querySelector('.fa-bars');
+    const navMenu = document.querySelector('header .navegacao-primaria');
+    
+    
+    activeMenu.addEventListener('click',()=>{
+        activeMenu.classList.toggle('fa-x');
+        navMenu.classList.toggle('ativado');
+    });
+
+}
+menuMobile();

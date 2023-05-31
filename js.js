58bca21e4fc9383a7e96d81c3be9cb9c58a28b49
AMentalCoderJ <josuesodre36@gmail.com>
2023-05-31 12:47:55 -0300

    1 commit

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-
+function escrevendoLetra() {
 
 
 function ativaLetra(elemento){
@@ -12,15 +12,68 @@ function ativaLetra(elemento){
     });
 }
 
-
 const titulo = document.querySelector('.digitando');
 ativaLetra(titulo);
 
-
-
+}
+escrevendoLetra();
 
 const ativaMenu = document.querySelector('.fa-bars');
 
 ativaMenu.addEventListener('click', ()=>{
     ativaMenu.classList.toggle('fa-x')
 })
+
+
+const divExperiencia = document.querySelectorAll('.experience_content div');
+const liExperiencia = document.querySelectorAll('.experience_content ul li');
+const divEducation = document.querySelectorAll('.education_content div');
+const liEducation = document.querySelectorAll('.education_content ul li');
+function sobreMim0(){
+
+
+function slideShow(index){
+    divExperiencia.forEach((div)=>{
+        div.classList.remove('ativo');
+    })
+    liExperiencia.forEach((botao)=>{
+        botao.classList.remove('ativo');
+    })
+    divExperiencia[index].classList.add('ativo');
+    liExperiencia[index].classList.add('ativo');
+}
+    liExperiencia.forEach((event, index)=>{
+        event.addEventListener('click', ()=>{
+            slideShow(index);
+        })
+    })
+}
+
+sobreMim0();
+divExperiencia[0].classList.add('ativo');
+liExperiencia[0].classList.add('ativo');
+
+function sobreMin(){
+function slideShow(index){
+    divEducation.forEach((div)=>{
+        div.classList.remove('ativo');
+    })
+    liEducation.forEach((botao)=>{
+        botao.classList.remove('ativo');
+    })
+    divEducation[index].classList.add('ativo');
+    liEducation[index].classList.add('ativo');
+}
+
+
+liEducation.forEach((event, index)=>{
+    event.addEventListener('click', ()=>{
+        slideShow(index);
+    })
+})
+
+}
+
+sobreMin();
+divEducation[0].classList.add('ativo');
+divEducation[0].classList.add('ativo');
